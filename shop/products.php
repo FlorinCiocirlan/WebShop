83a0e1aec5bb10709580a5df664e22017afa7779
Adrian-P0gar <pogar.adi@gmail.com>
2020-10-08 13:08:46 +0300

    show products

---
+++ shop/products.php
@@ -0,0 +1,40 @@
+<?php
+require_once "../Core/basecontroller.php";
+
+
+class ProductsController extends BaseController
+{
+
+
+    public function handleGet(): string
+    {
+
+        $as ='ceva';
+        $prod = "Acesta este products conroler";
+         $ds = $this->getAllProducts();
+        $this->templateData['prod'] = ['as'=> $as, "prod"=>$ds];
+
+
+
+        return "products";
+
+    }
+
+
+    public function handlePost(): string
+    {
+        // TODO: Implement handlePost() method.
+    }
+
+    public function getAllProducts():array
+    {
+        $connection = getConnection();
+        $query = "SELECT * FROM product";
+        $statement = $connection->prepare($query);
+        $statement->execute();
+        $resultset = $statement->fetchAll();
+        return $resultset;
+    }
+    }
+
+BaseController::run();
---
+++ shop/views/products.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <!-- End of Bootstrap components -->
+
+    <link rel="stylesheet" type="text/css" href="/static/css/custom.css"/>
+</head>
+<body>
+<h1> Products : </h1>
+<div id="products" class="row">
+
+    <?php
+
+    foreach ( $prod['prod'] as $product )
+    {
+        echo  ' <div class="col col-sm-12 col-md-6 col-lg-4"> 
+                    <div class="card" style=" width:450px;"> 
+                        <img class="card-img-top" style="height:400px; width:auto;" src="/images/'. $product["image"] .'" alt=""/> 
+                            <div class="card-header"> 
+                                    <h4 class="card-title">Product name: '. $product["name"].'</h4>                                                                
+                            </div>
+                               <div class="card-body">
+                                  <div class="card-text">
+                                   <p class="card-text"> '.$product["brand"]  .'   </p>
+                                          <p class="lead" >Price: '.$product["price"] .' USD</p>                                
+                                  </div>
+                                  <div class="card-text">
+                 <a class="btn btn-success" href="#">Add to cart</a>    
+                 <a class="btn btn-primary" href="#"> Details </a>                
+                                    </div>
+                               </div> 
+                    </div>
+                </div>';
+    }
+    ?>
+</div>
+
+</body>
+</html>
